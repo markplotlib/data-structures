deb08d6377ebafe05ca32f72794132e3678ae34b
markplotlib <markplotlib@gmail.com>
2020-06-16 23:36:24 -0700

    insert executable code into linked_list module

--- linklist/linked_list.py
+++ linklist/linked_list.py
@@ -4,3 +4,12 @@ class LinkedList:
     def __init__(self):
         self.head = None
         self.length = 0
+
+
+if __name__ == '__main__':
+    # construct linked list
+    chain = LinkedList()
+
+    # display length of uninitialized linked list (zero)
+    print("Linked list length is " + str(chain.length))
+
