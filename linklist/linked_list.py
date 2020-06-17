faf9aab076278ec929356c9e5d928d0175922a8f
markplotlib <markplotlib@gmail.com>
2020-06-17 15:25:23 -0700

    test ctors for Linked_List.length (no-args and 1-arg)

--- linklist/linked_list.py
+++ linklist/linked_list.py
@@ -1,6 +1,7 @@
 class LinkedList:
     def __init__(self, node=None):
         self.head = node
+        self.length = 0 if node is None else 1
 
 
 class Node:
--- linklist/test_linked_list.py
+++ linklist/test_linked_list.py
@@ -18,9 +18,28 @@ class MyTestCase(unittest.TestCase):
 
         # act
         ll1: object = ll.LinkedList(n1)
-        
+
         # assert
         self.assertIsNotNone(ll1.head)
+
+    def test_ctor_no_args_length_is_0(self):
+        # arrange
+
+        # act
+        ll0: object = ll.LinkedList()
+
+        # assert
+        self.assertEqual(ll0.length, 0)
+
+    def test_ctor_1_arg_length_is_1(self):
+        # arrange
+        n1 = ll.Node(123)
+
+        # act
+        ll1: object = ll.LinkedList(n1)
+
+        # assert
+        self.assertEqual(ll1.length, 1)
 
 
 if __name__ == '__main__':
