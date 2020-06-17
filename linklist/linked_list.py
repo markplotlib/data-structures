f52c23087471fc80cbe3aea64fce90b5c3cb4e17
markplotlib <markplotlib@gmail.com>
2020-06-17 14:56:41 -0700

    move small Node class into linked_list.py module

--- linklist/linked_list.py
+++ linklist/linked_list.py
@@ -1,6 +1,10 @@
-from node import Node
-
 class LinkedList:
     def __init__(self, node=None):
         self.head = node
         self.length = 0 if node is None else 1
+
+
+class Node:
+    def __init__(self, data):
+        self.data = data
+        self.next = None
