35b4c27257c5a5dc9b766dac40741ac346773777
markplotlib <markplotlib@gmail.com>
2020-06-17 15:21:25 -0700

    unit test ctor for 1-arg ctor

--- linklist/test_linked_list.py
+++ linklist/test_linked_list.py
@@ -4,8 +4,23 @@ import linked_list as ll
 
 class MyTestCase(unittest.TestCase):
     def test_ctor_no_args_head_is_none(self):
+        # arrange
+
+        # act
         ll0: object = ll.LinkedList()
+
+        # assert
         self.assertIsNone(ll0.head)
+
+    def test_ctor_1_arg_head_is_not_none(self):
+        # arrange
+        n1 = ll.Node(123)
+
+        # act
+        ll1: object = ll.LinkedList(n1)
+        
+        # assert
+        self.assertIsNotNone(ll1.head)
 
 
 if __name__ == '__main__':
