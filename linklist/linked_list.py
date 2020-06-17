9e9139b39dc7125a5b7852fcd54d02296a9524b1
markplotlib <markplotlib@gmail.com>
2020-06-17 09:21:54 -0700

    expand constructor to accommodate no-args default and node-arg

--- linklist/linked_list.py
+++ linklist/linked_list.py
@@ -1,15 +1,21 @@
 from node import Node
 
 class LinkedList:
-    def __init__(self):
-        self.head = None
-        self.length = 0
+    def __init__(self, node=None):
+        self.head = node
+        self.length = 0 if node is None else 1
 
 
 if __name__ == '__main__':
     # construct linked list
-    chain = LinkedList()
+    chain0 = LinkedList()
 
     # display length of uninitialized linked list (zero)
-    print("Linked list length is " + str(chain.length))
+    print("Empty chain has length of " + str(chain0.length))
 
+    # initialize linked list
+    print("New chain, containing sequence of digits of pi ...")
+    n3 = Node(3)
+    chain_pi = LinkedList(n3)
+    print("Pi chain has length of " + str(chain_pi.length))
+    print(chain_pi.head.data)
