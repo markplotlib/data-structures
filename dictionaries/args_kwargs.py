433e2cae3fd2370c50d5bf32958571230cd022ac
markplotlib <mark.chesney@gmail.com>
2021-01-22 22:26:33 -0800

    add *args func

--- dictionaries/args_kwargs.py
+++ dictionaries/args_kwargs.py
@@ -1,10 +1,14 @@
 # https://towardsdatascience.com/10-examples-to-master-args-and-kwargs-in-python-6f1e8cc30749
 # args and kwargs
 
-def add(a, b):
-    return a + b
+def add(*args):
+    total = 0
+    for i in args:
+        total += i
+    return total
 
 
 if __name__ == '__main__':
     print(add(3, 4))
-#    print(add(3, 4, 5))
+    print(add(3, 4, 5))
+    print(add(3, 4, 5, 6, 7))
