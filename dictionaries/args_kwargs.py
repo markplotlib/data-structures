7807af901f7d6b2e543634a1186ca461cb7ff90e
markplotlib <mark.chesney@gmail.com>
2021-01-22 22:24:46 -0800

    begin args-kwargs lesson

---
+++ dictionaries/args_kwargs.py
@@ -0,0 +1,10 @@
+# https://towardsdatascience.com/10-examples-to-master-args-and-kwargs-in-python-6f1e8cc30749
+# args and kwargs
+
+def add(a, b):
+    return a + b
+
+
+if __name__ == '__main__':
+    print(add(3, 4))
+#    print(add(3, 4, 5))
