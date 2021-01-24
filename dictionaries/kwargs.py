2deae398233339ced453b081b5bb0fde49b5f663
markplotlib <mark.chesney@gmail.com>
2021-01-24 10:54:24 -0800

    demo that args precede kwargs in func param definition

--- dictionaries/kwargs.py
+++ dictionaries/kwargs.py
@@ -1,9 +1,11 @@
 
-def arg_printer(a, b, option=True, **kwargs):
+def arg_printer(a, b, *args, option=True, **kwargs):
     print(a, b)
+    print(args)
     print(option)
     print(f'kwargs: {kwargs}')
 
+
 if __name__ == '__main__':
-    arg_printer(1, 2, param1=5, param2=6)
+    arg_printer(1, 2, 5, 'three, sir', param1=5, param2=6)
 
