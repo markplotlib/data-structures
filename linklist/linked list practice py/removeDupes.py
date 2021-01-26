4d17298a17e1e1d602d2908c0070a1c0b2906222
markplotlib <mark.chesney@gmail.com>
2021-01-26 10:28:16 -0800

    include drafted code for LL test examples

---
+++ linklist/linked list practice py/removeDupes.py
@@ -0,0 +1,28 @@
+# Definition for singly-linked list.
+class ListNode:
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
+
+
+# class Solution:
+# def deleteDuplicates(self, head: ListNode) -> ListNode:
+def deleteDuplicates(head: ListNode) -> ListNode:
+	if head == None:
+		return None
+	
+	n = head
+	
+	while n.next is not None:
+		if n.val == n.next.val:
+			n.next = n.next.next
+		n = n.next
+
+	return head
+
+
+if __name__ == '__main__':
+	c = ListNode(val=2)
+	b = ListNode(val=1, next=c)
+	a = ListNode(val=1, next=b)
+	deleteDuplicates(a)
