1744595daae27b6df39b2ecf89e34da6156f8f6a
markplotlib <mark.chesney@gmail.com>
2021-01-27 20:03:54 -0800

    attempt delete duplicate challenge

--- linklist/leetcode/deleteDuplicates.py
+++ linklist/leetcode/deleteDuplicates.py
@@ -1,4 +1,4 @@
-https://leetcode.com/problems/remove-duplicates-from-sorted-list/
+# https://leetcode.com/problems/remove-duplicates-from-sorted-list/
 
 # Definition for singly-linked list.
 class ListNode:
@@ -7,24 +7,40 @@ class ListNode:
         self.next = next
 
 
-# class Solution:
-# def deleteDuplicates(self, head: ListNode) -> ListNode:
-def deleteDuplicates(head: ListNode) -> ListNode:
-	if head == None:
-		return None
+class Solution:
+    def deleteDuplicates(self, head: ListNode) -> ListNode:
+        int_set = set()
+        cur = head
 
-	n = head
+        while cur.next is not None:
+            if cur.val not in int_set:
+                int_set.add(cur.val)
+            # advance to next node
+            cur = cur.next
+        # finished product: full, ordered set
 
-	while n.next is not None:
-		if n.val == n.next.val:
-			n.next = n.next.next
-		n = n.next
+        # cast set to list
+        list_int = list(int_set)
 
-	return head
+        # pop the value of the final tail, whose node gets next=None
+        tail = ListNode(val=list_int.pop(), next=None)
+        cur = tail
+        prev = None
+
+        while len(list_int) > 0:
+            prev = ListNode(
+                            val=list_int.pop(),
+                            next=cur)
+            cur = prev
+            # note: pop() decrements list length, so terminal condition will be met
+
+        return cur
 
 
 if __name__ == '__main__':
 	c = ListNode(val=2)
 	b = ListNode(val=1, next=c)
 	a = ListNode(val=1, next=b)
-	deleteDuplicates(a)
+
+    # soln = Solution()
+    # no_dupes = soln.deleteDuplicates(a)
