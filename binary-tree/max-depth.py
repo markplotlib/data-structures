08fd1c94f1d98b3bf62ae48c46b2160dc573db3d
markplotlib <mark.chesney@gmail.com>
2021-02-01 21:23:13 -0800

    initiate max-depth binary tree script

---
+++ binary-tree/max-depth.py
@@ -0,0 +1,17 @@
+# https://leetcode.com/problems/maximum-depth-of-binary-tree/
+
+# Definition for a binary tree node.
+class TreeNode:
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+
+class Solution:
+    def maxDepth(self, root: TreeNode) -> int:
+        """
+        >>> Solution().maxDepth(root=None)
+        0
+        """
+        if root is None:
+            return 0
