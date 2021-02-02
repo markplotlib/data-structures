353f04f56f2ad0c0907169790df15107ee6cbd5f
markplotlib <mark.chesney@gmail.com>
2021-02-01 21:36:55 -0800

    pass all doctest cases

--- binary-tree/max-depth.py
+++ binary-tree/max-depth.py
@@ -12,6 +12,15 @@ class Solution:
         """
         >>> Solution().maxDepth(root=None)
         0
+        >>> Solution().maxDepth(root=TreeNode(1))
+        1
+        >>> Solution().maxDepth(root=TreeNode(1,TreeNode(2)))
+        2
+        >>> Solution().maxDepth(TreeNode(3,TreeNode(9,None,None),TreeNode(20,TreeNode(15,None,None),TreeNode(7,None,None))))
+        3
         """
         if root is None:
             return 0
+        else:
+            return 1 + max(Solution().maxDepth(root.left),
+                           Solution().maxDepth(root.right))
